00bdc48dea8d59a1e3602d81a8cace9948b0a250
Markus Heinemann <markus.heinemann@accenture.com>
2020-11-11 14:17:42 +0100

    Add arithmetic operators to Lesson1

--- src/tech/heinemann/kickstart/Lesson1.java
+++ src/tech/heinemann/kickstart/Lesson1.java
@@ -8,7 +8,29 @@ public class Lesson1 {
         int number = 123;
         char a = 'a';
 
+        int x = 1;
+        int y = 2;
 
-        System.out.println(hello);
+        int result = x + y;
+        int result2 = x - y;
+        int result3 = x * y;
+        int result4 = x / y;
+        int result5 = (x + y) * 2;
+
+
+        System.out.println(isBoolean);
+        System.out.println(number);
+        System.out.println(a);
+        System.out.println("");
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println("");
+
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
     }
 }
